c06d0442df88ef2800228d7082f82c5623844566
Thys Meintjes <sthysel@gmail.com>
2011-06-02 15:22:36 +0200

    Task children added

--- DueList/src/main/java/thyscom/duelist/TaskChildren.java
+++ DueList/src/main/java/thyscom/duelist/TaskChildren.java
@@ -8,6 +8,8 @@ import java.util.List;
 import javax.swing.JSpinner;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import org.joda.time.DateTime;
+import org.joda.time.Interval;
 import org.openide.nodes.Children;
 import org.openide.nodes.Node;
 import org.openide.util.Lookup;
@@ -33,15 +35,57 @@ public class TaskChildren extends Children.Array implements ChangeListener, Prop
 
     @Override
     public Collection<Node> initCollection() {
-        ArrayList<TaskNode> taskNodes = new ArrayList<TaskNode>();
 
+        ArrayList<Task> dueTasks = new ArrayList<Task>();
+
+        // populate the duetasks list
+        getAllDueTasksForGivenWeek(dueTasks);
+
+        // wrap tasks in nodes and register propertychangelistener
+        Collection<Node> dueNodes = new ArrayList<Node>(dueTasks.size());
+        for (Task task : dueTasks) {
+            task.addPropertyChangeListener(this);
+            dueNodes.add(new TaskNode(task));
+        }
+
+        return dueNodes;
+    }
+
+    private void getAllDueTasksForGivenWeek(List<Task> dueTasks) {
+        Interval interval = getInterval();
         if (taskManager != null) {
             List<Task> parents = taskManager.getAllParentTasks();
             for (Task task : parents) {
-                taskNodes.add(new TaskNode(task));
+                findDueTasks(interval, task, dueTasks);
             }
         }
-        return taskNodes;
+    }
+
+    /*
+     * Recursively populate the duetask list
+     */
+    private void findDueTasks(Interval interval, Task task, List<Task> dueTasks) {
+        DateTime duedate = new DateTime(task.getDueDate());
+        if (interval.contains(duedate)) {
+            dueTasks.add(task);
+            for (Task child : task.getChildren()) {
+                findDueTasks(interval, child, dueTasks);
+            }
+        }
+    }
+
+    /**
+     * Given the week, get the period of the week
+     * @return 
+     */
+    private Interval getInterval() {
+        // get tdoay, set the week to the spinner value
+        // set the day to thebegiining of the week to get the start time
+        // add a week to get the end time
+        DateTime now = new DateTime();
+        DateTime begin = now.withWeekyear((Integer) spinner.getValue());
+        DateTime end = now.plusDays(7);
+        return new Interval(begin.getMillis(), end.getMillis());
     }
 
     @Override
@@ -56,12 +100,12 @@ public class TaskChildren extends Children.Array implements ChangeListener, Prop
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
-        if (isThisATaskEvent(evt)) {
+        if (thisIsATaskEvent(evt)) {
             updateNodes();
         }
     }
 
-    private boolean isThisATaskEvent(PropertyChangeEvent evt) {
+    private boolean thisIsATaskEvent(PropertyChangeEvent evt) {
         return (evt.getSource() instanceof TaskManager)
                 && (evt.getPropertyName().equals(TaskManager.PROPERTY_TASK_ADD) || evt.getPropertyName().equals(TaskManager.PROPERTY_TASK_REMOVE));
     }
