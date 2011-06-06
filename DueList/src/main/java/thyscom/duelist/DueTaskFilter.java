21c24df0be35409ecdd3bfdafcb2f3c4fd6ea55c
sthysel <sthysel@gmail.com>
2011-06-06 23:07:41 +0200

    Factored out filter logic into a filter class.

---
+++ DueList/src/main/java/thyscom/duelist/DueTaskFilter.java
@@ -0,0 +1,63 @@
+package thyscom.duelist;
+
+import java.util.List;
+import org.joda.time.DateTime;
+import org.joda.time.Interval;
+import thyscom.taskmodel.api.Task;
+import thyscom.taskmodel.api.TaskManager;
+
+/**
+ * Filters through all tasks looking for due tasks for the given week
+ * @author thys
+ */
+public class DueTaskFilter {
+
+    TaskManager taskManager;
+    
+    public DueTaskFilter(TaskManager taskManager) {
+        this.taskManager = taskManager;
+    }
+
+    
+    /**
+     * Returns dueTasks for the week
+     * @param week
+     * @param dueTasks 
+     */
+    public void getAllDueTasksForGivenWeek(int week, List<Task> dueTasks) {
+        Interval interval = getInterval(week);
+        if (taskManager != null) {
+            List<Task> parents = taskManager.getAllParentTasks();
+            for (Task task : parents) {
+                findDueTasks(interval, task, dueTasks);
+            }
+        }
+    }
+
+    /*
+     * Recursively populate the duetask list
+     */
+    private void findDueTasks(Interval interval, Task task, List<Task> dueTasks) {
+        DateTime duedate = new DateTime(task.getDueDate());
+        if (interval.contains(duedate)) {
+            dueTasks.add(task);
+            for (Task child : task.getChildren()) {
+                findDueTasks(interval, child, dueTasks);
+            }
+        }
+    }
+
+    /**
+     * Given the week, get the period of the week
+     * @return 
+     */
+    private Interval getInterval(int week) {
+        // get today, set the week to the spinner value
+        // set the day to thebegiining of the week to get the start time
+        // add a week to get the end time
+        DateTime now = new DateTime();
+        DateTime begin = now.withWeekyear(week);
+        DateTime end = begin.plusDays(7);
+        return new Interval(begin, end);
+    }
+}
--- DueList/src/main/java/thyscom/duelist/TaskChildren.java
+++ DueList/src/main/java/thyscom/duelist/TaskChildren.java
@@ -4,12 +4,9 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 import javax.swing.JSpinner;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
-import org.joda.time.DateTime;
-import org.joda.time.Interval;
 import org.openide.nodes.Children;
 import org.openide.nodes.Node;
 import org.openide.util.Lookup;
@@ -22,24 +19,26 @@ import thyscom.taskmodel.api.TaskManager;
  * @author thys
  */
 public class TaskChildren extends Children.Array implements ChangeListener, PropertyChangeListener {
-
+    
     private JSpinner spinner;
     private TaskManager taskManager;
-
+    private DueTaskFilter dueFilter;
+    
     public TaskChildren(JSpinner spinner) {
         this.spinner = spinner;
         spinner.addChangeListener(this);
         taskManager = Lookup.getDefault().lookup(TaskManager.class);
+        dueFilter = new DueTaskFilter(taskManager);
         taskManager.addPropertyChangeListener(this);
     }
-
+    
     @Override
     public Collection<Node> initCollection() {
-
+        
         ArrayList<Task> dueTasks = new ArrayList<Task>();
 
         // populate the duetasks list
-        getAllDueTasksForGivenWeek(dueTasks);
+        dueFilter.getAllDueTasksForGivenWeek((Integer) spinner.getValue(), dueTasks);
 
         // wrap tasks in nodes and register propertychangelistener
         Collection<Node> dueNodes = new ArrayList<Node>(dueTasks.size());
@@ -47,57 +46,20 @@ public class TaskChildren extends Children.Array implements ChangeListener, Prop
             task.addPropertyChangeListener(this);
             dueNodes.add(new TaskNode(task));
         }
-
+        
         return dueNodes;
     }
-
-    private void getAllDueTasksForGivenWeek(List<Task> dueTasks) {
-        Interval interval = getInterval();
-        if (taskManager != null) {
-            List<Task> parents = taskManager.getAllParentTasks();
-            for (Task task : parents) {
-                findDueTasks(interval, task, dueTasks);
-            }
-        }
-    }
-
-    /*
-     * Recursively populate the duetask list
-     */
-    private void findDueTasks(Interval interval, Task task, List<Task> dueTasks) {
-        DateTime duedate = new DateTime(task.getDueDate());
-        if (interval.contains(duedate)) {
-            dueTasks.add(task);
-            for (Task child : task.getChildren()) {
-                findDueTasks(interval, child, dueTasks);
-            }
-        }
-    }
-
-    /**
-     * Given the week, get the period of the week
-     * @return 
-     */
-    private Interval getInterval() {
-        // get today, set the week to the spinner value
-        // set the day to thebegiining of the week to get the start time
-        // add a week to get the end time
-        DateTime now = new DateTime();
-        DateTime begin = now.withWeekyear((Integer) spinner.getValue());
-        DateTime end = begin.plusDays(7);
-        return new Interval(begin, end);
-    }
-
+    
     @Override
     public void stateChanged(ChangeEvent e) {
         updateNodes();
     }
-
+    
     private void updateNodes() {
         remove(getNodes());
         add(initCollection().toArray(new Node[]{}));
     }
-
+    
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
 //        if (thisIsATaskEvent(evt)) {
@@ -105,7 +67,7 @@ public class TaskChildren extends Children.Array implements ChangeListener, Prop
 //        }
         updateNodes();
     }
-
+    
     private boolean thisIsATaskEvent(PropertyChangeEvent evt) {
         return (evt.getSource() instanceof TaskManager)
                 && (evt.getPropertyName().equals(TaskManager.PROPERTY_TASK_ADD) || evt.getPropertyName().equals(TaskManager.PROPERTY_TASK_REMOVE));
